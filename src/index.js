3b57253ca0aa1435529645d4e4819ba3e269e728
scian0204 <scian0204@gmail.com>
2023-04-27 16:48:10 +0900

    Add Chapter09
    9주차 chapter09 실습

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // setInterval(()=>{
@@ -13,7 +14,8 @@ root.render(
     {/* <App /> */}
     {/* <NotificationList /> */}
     {/* <Accommodate /> */}
-    <ConfirmButton />
+    {/* <ConfirmButton /> */}
+    <LandingPage />
   </React.StrictMode>
 );
 // }, 1000);
