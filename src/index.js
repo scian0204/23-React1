8caa679752259cc4832bb644436de90fc365204c
scian0204 <scian0204@gmail.com>
2023-05-04 16:48:01 +0900

    Add Chapter11
    10주차 chapter11 실습

--- src/index.js
+++ src/index.js
@@ -2,25 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Accommodate from './chapter_07/Accommodate';
-import ConfirmButton from './chapter_08/ConfirmButton';
-import LandingPage from './chapter_09/LandingPage';
-import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// setInterval(()=>{
 root.render(
   <React.StrictMode>
-    {/* <Clock /> */}
-    {/* <App /> */}
-    {/* <NotificationList /> */}
-    {/* <Accommodate /> */}
-    {/* <ConfirmButton /> */}
-    {/* <LandingPage /> */}
-    <AttendanceBook />
+    <SignUp />
   </React.StrictMode>
 );
-// }, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
